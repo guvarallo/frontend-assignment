159c586d34f032bfbd88ac1be48325f4f4ce1a79
Gustavo Varallo <gustavo.varallo@live.com>
2021-10-01 15:17:18 -0300

    refactor: change api call to return results directly

--- src/api/MovieAPI.js
+++ src/api/MovieAPI.js
@@ -2,9 +2,9 @@ export const fetchMovies = async () => {
   const apiKey = '7315ec59ea2264da1fa4f4eb8d647853'
 
   return await fetch(
-    `https://api.themoviedb.org/3/movie/76341?api_key=${apiKey}`
+    `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`
   )
     .then(res => res.json())
-    .then(data => data)
+    .then(data => data.results)
     .catch(err => console.error(err))
 }
