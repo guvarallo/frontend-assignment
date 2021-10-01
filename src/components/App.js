f249e0052ec384ab61a8c992da37b037db325b6c
Gustavo Varallo <gustavo.varallo@live.com>
2021-10-01 15:17:59 -0300

    feat: implement movie cards

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,67 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react'
+import styled from 'styled-components'
+import { fetchMovies } from '../api/MovieAPI'
+import logo from '../images/logo.svg'
 
-const App = () => (
-	<img src={logo} alt="Timescale" />
-)
+const AppContainer = styled.div`
+  max-width: 75rem;
+`
 
-export default App;
+const ImageWrapper = styled.div`
+  width: 17.625rem;
+  height: 21.75rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 1.625rem;
+  background: #ffffff;
+  border: 1px solid #e1e3e6;
+  box-sizing: border-box;
+  box-shadow: 1px 2px 8px 2px rgba(0, 0, 0, 0.1);
+  border-radius: 8px;
+`
+
+const Image = styled.img`
+  width: 17.5rem;
+  height: 18.875rem;
+  object-fit: cover;
+  border-radius: 8px;
+`
+
+const Title = styled.p`
+  font-family: Inter;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 1rem;
+`
+
+const App = () => {
+  const [movies, setMovies] = useState([])
+  const imageURLPrefix = 'https://image.tmdb.org/t/p/original'
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchMovies()
+      setMovies(data)
+    }
+    fetchData()
+  }, [])
+
+  return (
+    <AppContainer>
+      <img src={logo} alt='Timescale' />
+      {movies.map(movie => (
+        <ImageWrapper key={movie.overview}>
+          <Image
+            src={imageURLPrefix + movie.poster_path}
+            alt={movie.original_title}
+          />
+          <Title>{movie.original_title}</Title>
+        </ImageWrapper>
+      ))}
+    </AppContainer>
+  )
+}
+
+export default App
