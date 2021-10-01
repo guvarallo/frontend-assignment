b9b316a07c78353414f8f71534887a10c197fc88
Gustavo Varallo <gustavo.varallo@live.com>
2021-10-01 15:42:34 -0300

    feat: add avarage rate circle

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,18 @@ import logo from '../images/logo.svg'
 
 const AppContainer = styled.div`
   max-width: 75rem;
+  display: flex;
+  flex-wrap: wrap;
+  font-family: Inter;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 1rem;
 `
 
 const ImageWrapper = styled.div`
   width: 17.625rem;
   height: 21.75rem;
+  position: relative;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -26,15 +33,32 @@ const Image = styled.img`
   width: 17.5rem;
   height: 18.875rem;
   object-fit: cover;
-  border-radius: 8px;
+  border-radius: 7px 7px 0px 0px;
 `
 
-const Title = styled.p`
-  font-family: Inter;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 1rem;
+const VoteAverageWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  width: 34px;
+  height: 34px;
+  left: 14px;
+  top: 14px;
+  z-index: 1000;
+
+  background: #ffffff;
+  border: 1px solid #000000;
+  box-sizing: border-box;
+  border-radius: 50%;
 `
+
+const VoteAverageText = styled.p`
+  font-size: 12px;
+  font-weight: bold;
+`
+
+const Title = styled.p``
 
 const App = () => {
   const [movies, setMovies] = useState([])
@@ -48,19 +72,26 @@ const App = () => {
     fetchData()
   }, [])
 
+  console.log(movies)
+
   return (
-    <AppContainer>
+    <>
       <img src={logo} alt='Timescale' />
-      {movies.map(movie => (
-        <ImageWrapper key={movie.overview}>
-          <Image
-            src={imageURLPrefix + movie.poster_path}
-            alt={movie.original_title}
-          />
-          <Title>{movie.original_title}</Title>
-        </ImageWrapper>
-      ))}
-    </AppContainer>
+      <AppContainer>
+        {movies.map(movie => (
+          <ImageWrapper key={movie.overview}>
+            <VoteAverageWrapper>
+              <VoteAverageText>{movie.vote_average}</VoteAverageText>
+            </VoteAverageWrapper>
+            <Image
+              src={imageURLPrefix + movie.poster_path}
+              alt={movie.original_title}
+            />
+            <Title>{movie.original_title}</Title>
+          </ImageWrapper>
+        ))}
+      </AppContainer>
+    </>
   )
 }
 
