fd3f8af527d414643b5767cb48e40fbfba7653e0
Gustavo Varallo <gustavo.varallo@live.com>
2021-10-01 17:34:01 -0300

    feat: apply Modal

--- src/components/App.js
+++ src/components/App.js
@@ -2,20 +2,18 @@ import { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import { fetchMovies } from '../api/MovieAPI'
 import logo from '../images/logo.svg'
+import { GlobalStyle } from '../styles'
+import { Modal } from './Modal'
 
 const AppContainer = styled.div`
   max-width: 75rem;
   display: flex;
   flex-wrap: wrap;
-  font-family: Inter;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 1rem;
 `
 
 const ImageWrapper = styled.div`
   width: 17.625rem;
-  height: 21.75rem;
+  height: 22.75rem;
   position: relative;
   display: flex;
   flex-direction: column;
@@ -24,16 +22,20 @@ const ImageWrapper = styled.div`
   margin: 1.625rem;
   background: #ffffff;
   border: 1px solid #e1e3e6;
-  box-sizing: border-box;
   box-shadow: 1px 2px 8px 2px rgba(0, 0, 0, 0.1);
   border-radius: 8px;
+  cursor: pointer;
 `
 
 const Image = styled.img`
   width: 17.5rem;
-  height: 18.875rem;
+  height: 19.5rem;
   object-fit: cover;
   border-radius: 7px 7px 0px 0px;
+`
+
+const Title = styled.p`
+  padding: 1rem;
 `
 
 const VoteAverageWrapper = styled.div`
@@ -45,7 +47,6 @@ const VoteAverageWrapper = styled.div`
   height: 34px;
   left: 14px;
   top: 14px;
-  z-index: 1000;
 
   background: #ffffff;
   border: 1px solid #000000;
@@ -58,11 +59,12 @@ const VoteAverageText = styled.p`
   font-weight: bold;
 `
 
-const Title = styled.p``
+export const IMAGE_URL_PREFIX = 'https://image.tmdb.org/t/p/original'
 
 const App = () => {
   const [movies, setMovies] = useState([])
-  const imageURLPrefix = 'https://image.tmdb.org/t/p/original'
+  const [openModal, setOpenModal] = useState(false)
+  const [movieIndex, setMovieIndex] = useState(0)
 
   useEffect(() => {
     const fetchData = async () => {
@@ -72,24 +74,38 @@ const App = () => {
     fetchData()
   }, [])
 
+  const handleOpenModal = index => {
+    setOpenModal(!openModal)
+    setMovieIndex(index)
+  }
+
+  console.log(openModal)
+
   console.log(movies)
 
   return (
     <>
+      <GlobalStyle />
       <img src={logo} alt='Timescale' />
       <AppContainer>
-        {movies.map(movie => (
-          <ImageWrapper key={movie.overview}>
+        {movies.map((movie, index) => (
+          <ImageWrapper onClick={() => handleOpenModal(index)}>
             <VoteAverageWrapper>
               <VoteAverageText>{movie.vote_average}</VoteAverageText>
             </VoteAverageWrapper>
             <Image
-              src={imageURLPrefix + movie.poster_path}
+              src={IMAGE_URL_PREFIX + movie.poster_path}
               alt={movie.original_title}
             />
             <Title>{movie.original_title}</Title>
           </ImageWrapper>
         ))}
+        <Modal
+          movie={movies[movieIndex]}
+          openModal={openModal}
+          setOpenModal={setOpenModal}
+          style={{ backgroundColor: 'fff', zIndex: -1 }}
+        />
       </AppContainer>
     </>
   )
